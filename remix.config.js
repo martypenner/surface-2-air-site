82c91cfb9a057fa2fb4292a9f95d6477adfe3f82
Marty Penner <martypenner@users.noreply.github.com>
2022-08-30 00:12:55 +0000

    chore: update to remix indie stack

--- remix.config.js
+++ remix.config.js
@@ -4,11 +4,8 @@ const { redirects } = require('./app/redirects.json');
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 
   routes: async (defineRoutes) => {
     // If you need to do async work, do it before calling `defineRoutes`, we use
