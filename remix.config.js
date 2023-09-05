9d15e644bbc7415b89e463d782a568646005fc25
Marty Penner <martypenner@users.noreply.github.com>
2023-09-04 22:23:02 -0400

    fix: remove old redirects

--- remix.config.js
+++ remix.config.js
@@ -1,21 +1,7 @@
-const { redirects } = require('./app/redirects.json');
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
-
-  routes: async (defineRoutes) => {
-    // If you need to do async work, do it before calling `defineRoutes`, we use
-    // the call stack of `route` inside to set nesting.
-
-    // todo: this isn't working for some reason
-    return defineRoutes((route) => {
-      redirects.forEach(([oldUrl]) => {
-        route(oldUrl, 'redirects.tsx');
-      });
-    });
-  },
 };
